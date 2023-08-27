bb0a6b26442e097db6661bc15823687536dc4956
Maayon Thayaparan <maayon.t.thayaparan@gmail.com>
2023-08-27 01:12:20 -0400

    adding restapis to get dealers and review from cloudant then routing to new views

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -60,7 +60,55 @@ class CarModel(models.Model):
         return f"{self.make.name} {self.name} - {self.year}"
 
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+# <HINT> Plain Python class `CarDealer` to hold dealer data. Data comes from Cloundant external service
 
+class CarDealer:
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
+class DealerReview:
+    def __init__(self, id, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year, sentiment):
+        # Review id
+        self.id = id
+        # Review Name
+        self.name = name
+        # Review Dealership
+        self.dealership = dealership
+        # Review Review
+        self.review = review
+        # Review Purchase
+        self.purchase = purchase
+        # Review Purchase Date
+        self.purchase_date = purchase_date
+        # Review Car Make
+        self.car_make = car_make
+        # Review Car Model
+        self.car_model = car_model
+        # Review Car Year
+        self.car_year = car_year
+        # Review Sentiment
+        self.sentiment = sentiment
+
+
